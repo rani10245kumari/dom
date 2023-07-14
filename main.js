4a2c8351748e31940b1cbc0993df7a1bd0438346
rani10245kumari <131008023+rani10245kumari@users.noreply.github.com>
2023-07-14 19:20:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ var randomNO = random(100);
 var highscore = null;
 
 function PlayAgain() {
+    intialValue=100;
     document.body.style.backgroundColor = "#003333"
-    document.querySelector('.chances').textContent = 100
+    document.querySelector('.chances').textContent = intialValue
     document.querySelector('.box').textContent = '?'
     document.getElementById('selectNo').value = ''
     document.querySelector(".msg").textContent = 'Start Guessing...'
